9bbf4e24c75cc32da77e938748dfb3074ac97632
Maksim133 <pravo24kr2008@gmail.com>
2023-02-17 20:37:00 +0700

    Added completion of 2 tasks on the course

--- js/functions.js
+++ js/functions.js
@@ -1,55 +1,46 @@
-function stringLength (text, сharacters) {
-  if (text.length > сharacters) {
-    return 'Результат: false — строка не проходит';
-  }
-  return 'Результат: true - строка проходит по длине';
-}
+const checkingLengthString = (text, сharacters) => text.length <= сharacters;
 
-console.log(stringLength('проверяемая строка', 10));
+checkingLengthString('проверяемая строка', 10);
 
 function checkPalindrome (stringToTest) {
-  invertedString = stringToTest.split('').reverse().join('');
-  if (invertedString.toLowerCase().replace(/\s/g,'') == stringToTest.toLowerCase().replace(/\s/g,'')) {
+  const invertedString = stringToTest.split('').reverse().join('');
+  if (invertedString.toLowerCase().replace(/\s/g,'') === stringToTest.toLowerCase().replace(/\s/g,'')) {
     return 'строка является палиндромом';
   }
   return 'это не палиндром';
 }
 
-console.log(checkPalindrome('Леша на полке клопа нашел'));
+checkPalindrome('Леша на полке клопа нашел');
 
 function NumbersFromString (stringToTest) {
   if (Number.isInteger(stringToTest)) {
     return Math.abs(stringToTest);
   }
-  numbers = stringToTest.replace(/[^0-9\s]/gi,'').split(' ').join('');
+  const numbers = stringToTest.replace(/[^0-9\s]/gi,'').split(' ').join('');
   return Number(numbers);
 }
 
-console.log(NumbersFromString('1 кефир, 0.5 батона'));
+NumbersFromString('1 кефир, 0.5 батона');
 
 function StringToFileAddress (originalString, minimumLength, additionalCharacters) {
 
-  if (minimumLength <= originalString.length) {
+  const characters = minimumLength - originalString.length;
+
+  if (additionalCharacters.length >= characters) {
+    additionalCharacters = additionalCharacters.substring (0, characters);
+    originalString = additionalCharacters + originalString;
     return originalString;
-  } else if (minimumLength > originalString.length) {
-    const characters = minimumLength - originalString.length;
-    if (additionalCharacters.length >= characters) {
-      additionalCharacters = additionalCharacters.substring (0, characters);
-      // eslint-disable-next-line no-return-assign
-      return originalString = additionalCharacters + originalString;
-    } else if (additionalCharacters.length < characters) {
-      // eslint-disable-next-line eqeqeq
-      while (originalString.length != minimumLength) { /* empty */
-        originalString = additionalCharacters + originalString;
-        if (additionalCharacters.length != 1) {
-          additionalCharacters = additionalCharacters.substring (0, additionalCharacters.length - 1);
-        }
+  } else if (additionalCharacters.length < characters) {
+
+    while (originalString.length !== minimumLength) {
+      originalString = additionalCharacters + originalString;
+      if (additionalCharacters.length !== 1) {
+        additionalCharacters = additionalCharacters.substring (0, additionalCharacters.length - 1);
       }
     }
     return originalString;
   }
-
+  return originalString;
 }
 
-// eslint-disable-next-line no-console
-console.log(StringToFileAddress('qwerty', 4, '0'));
+StringToFileAddress('q', 4, 'we');
