078a5fb8601c703e35ca28274045ef9d633d6770
Maksim133 <pravo24kr2008@gmail.com>
2023-03-09 17:05:28 +0700

    fixed syntax errors

--- js/functions.js
+++ js/functions.js
@@ -1,32 +1,32 @@
-const checkStringLength = (text, сharacters) => text.length <= сharacters;
+function checkStringLength(text, сharacters) {
+  return text.length <= сharacters;
+}
 
 checkStringLength('проверяемая строка', 10);
 
-const checkPalindrome = (stringToTest) => {
+function checkPalindrome(stringToTest) {
   const invertedString = stringToTest.split('').reverse().join('');
-  return (invertedString.toLowerCase().replace(/\s/g,'') === stringToTest.toLowerCase().replace(/\s/g,''));
-};
-
-
+  return (invertedString.toLowerCase().replace(/\s/g, '') === stringToTest.toLowerCase().replace(/\s/g, ''));
+}
 
 checkPalindrome('Леша на полке клопа нашел');
 
-const getNumbersFromAString = (stringToTest) => {
+function getNumbersFromAString(stringToTest) {
   if (Number.isInteger(stringToTest)) {
     return Math.abs(stringToTest);
   }
-  const numbers = stringToTest.replace(/[^0-9]/g,'');
+  const numbers = stringToTest.replace(/[^0-9]/g, '');
   return Number(numbers);
-};
+}
 
 getNumbersFromAString('агент 007');
 
-const getFileAddress = (originalString, minimumLength, additionalCharacters) => {
+function getFileAddress(originalString, minimumLength, additionalCharacters) {
 
   const characters = minimumLength - originalString.length;
 
   if (additionalCharacters.length >= characters) {
-    additionalCharacters = additionalCharacters.substring (0, characters);
+    additionalCharacters = additionalCharacters.substring(0, characters);
     originalString = additionalCharacters + originalString;
     return originalString;
   }
@@ -35,11 +35,11 @@ const getFileAddress = (originalString, minimumLength, additionalCharacters) => 
     while (originalString.length !== minimumLength) {
       originalString = additionalCharacters + originalString;
       if (additionalCharacters.length !== 1) {
-        additionalCharacters = additionalCharacters.substring (0, additionalCharacters.length - 1);
+        additionalCharacters = additionalCharacters.substring(0, additionalCharacters.length - 1);
       }
     }
     return originalString;
   }
   return originalString;
-};
+}
 getFileAddress('q', 4, 'we');
