2ad1991c7405ba862413c9bf8e06d4d0347b7c94
Keks <keks@htmlacademy.ru>
2023-03-31 14:06:06 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/thumbnail-rendering.js
+++ 9/js/thumbnail-rendering.js
@@ -1,16 +1,20 @@
 import {storageMoreDetails} from './create-comments.js';
 
 const pictureTemplate = document.querySelector('#picture').content;
-const dataToFill = storageMoreDetails();
+const dataStores = storageMoreDetails();
 const pictureListFragment = document.createDocumentFragment();
 const pictureListElement = document.querySelector('#picture');
 
-dataToFill.forEach(({url, likes, comment}) => {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = `${url}.jpg`;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comment.message;
-  pictureListFragment.append(pictureElement);
-});
+const renderPicture = () => {
+  dataStores.forEach(({url, likes, comment}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = `${url}.jpg`;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comment.message;
+    pictureListFragment.append(pictureElement);
+  });
+  pictureListElement.append(pictureListFragment);
+  return pictureListElement;
+};
 
-pictureListElement.append(pictureListFragment);
+export {renderPicture};
