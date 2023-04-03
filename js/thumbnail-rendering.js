2d1ec1d1b01a8a69211d89b841717d30d7e54aed
Maksim133 <pravo24kr2008@gmail.com>
2023-04-03 16:28:24 +0700

    module7-task1

--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -1,14 +1,14 @@
 import {storageMoreDetails} from './create-comments.js';
 
-const pictureTemplate = document.querySelector('#picture').content;
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const dataStores = storageMoreDetails();
 const pictureListFragment = document.createDocumentFragment();
-const pictureListElement = document.querySelector('#picture');
+const pictureListElement = document.querySelector('.pictures');
 
 const renderPicture = () => {
   dataStores.forEach(({url, likes, comment}) => {
     const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = `${url}.jpg`;
+    pictureElement.querySelector('.picture__img').src = `./${url}.jpg`;
     pictureElement.querySelector('.picture__likes').textContent = likes;
     pictureElement.querySelector('.picture__comments').textContent = comment.message;
     pictureListFragment.append(pictureElement);
