85a343108bc357ff763db03fefe5aca5b3464e7e
Maksim133 <pravo24kr2008@gmail.com>
2023-04-06 21:20:05 +0700

    module8-task2

--- js/full-size-image.js
+++ js/full-size-image.js
@@ -4,15 +4,51 @@ const bigPicture = document.querySelector('.big-picture');
 const pictureListElement = document.querySelector('.pictures');
 const commentListElement = document.querySelector('.social__comments');
 const pictureTemplate = document.querySelector('.social__comment');
-const pictureListFragment = document.createDocumentFragment();
-const commentsCount = document.querySelector('.social__comment-count');
 const downloadMoreButton = document.querySelector('.comments-loader');
 const body = document.querySelector('body');
 const buttonCancel = document.querySelector('.big-picture__cancel');
+const COMMENTS_PER_PORTION = 5;
+const commentsLoader = document.querySelector('.comments-loader');
+const socialCommentsCount = document.querySelector('.social__comment-count');
+
+let numberOfMessagesShown = 0;
+let allMessage = [];
+
+const renderComments = (comment) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.social__picture').src = comment.avatar;
+  pictureElement.querySelector('.social__picture').alt = comment.name;
+  pictureElement.querySelector('.social__text').textContent = comment.message;
+  return pictureElement;
+};
+
+const showMessages = (comments) => {
+
+  numberOfMessagesShown += COMMENTS_PER_PORTION;
+  if (numberOfMessagesShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    numberOfMessagesShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < numberOfMessagesShown; i++) {
+    const commentElement = renderComments(comments[i]);
+    fragment.append(commentElement);
+  }
+  commentListElement.innerHTML = '';
+  commentListElement.append(fragment);
+  socialCommentsCount.innerHTML = `${numberOfMessagesShown } из <span class="comments-count">${ comments.length }</span> комментариев</div>`;
+};
+
+downloadMoreButton.addEventListener('click', () => {
+  showMessages(allMessage);
+});
 
 const onFilterChange = () => {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
+  numberOfMessagesShown = 0;
 };
 
 buttonCancel.addEventListener('click', onFilterChange);
@@ -23,22 +59,8 @@ document.addEventListener('keydown', (esc) => {
   }
 });
 
-const renderComments = (comments) => {
-  comments.forEach(({avatar, name, message}) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.social__picture').src = `./${avatar}`;
-    pictureElement.querySelector('.social__picture').alt = name;
-    pictureElement.querySelector('.social__text').textContent = message;
-    pictureListFragment.append(pictureElement);
-  });
-  commentListElement.append(pictureListFragment);
-  return pictureListElement;
-};
-
 const showFullSizePicture = () => {
   bigPicture.classList.remove('hidden');
-  commentsCount.classList.add('hidden');
-  downloadMoreButton.classList.add('hidden');
   body.classList.add('modal-open');
 };
 
@@ -47,8 +69,9 @@ const renderFullSizePicture = (picture) => {
   bigPicture.querySelector('.social__caption').textContent = picture.description;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.comments-count').textContent = picture.comment.length;
-  renderComments(picture.comment);
+  showMessages(picture.comment);
   showFullSizePicture();
+  allMessage = picture.comment;
 };
 
 const renderPictures = () => {
